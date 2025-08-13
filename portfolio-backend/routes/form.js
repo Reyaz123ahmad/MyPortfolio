2bb65606a903cd455555d02a03399302525bfb62
Reyaz Ahmad <reyazahmad40544@gmail.com>
2025-08-13 11:54:39 +0530

    backend error fix in route

--- portfolio-backend/routes/form.js
+++ portfolio-backend/routes/form.js
@@ -6,11 +6,10 @@ const router=express.Router();
 const {createForm}=require('../controllers/createForm')
 const {getForms}=require('../controllers/getForms')
 const {updateForm}=require('../controllers/updateForm')
-const {postAttendence,getAttendence}=require('../controllers/postAttendence')
 
 router.post('/createForm',createForm);
 router.get('/getForm',getForms)
 router.put('/updateform/:id',updateForm);
-router.post('/postattendence',postAttendence)
-router.get('/getattendence',getAttendence)
+
+
 module.exports=router
